5d512e442a6acf1baa60582ed45d625530d42866
Jitse Lambrichts <lambrichts.j.99@gmail.com>
2025-05-24 17:40:45 +0200

    Laatste-nieuwtjes ook in orde

--- resources/views/nieuws.blade.php
+++ resources/views/nieuws.blade.php
@@ -3,22 +3,20 @@
 @section('content')
     <div class="wit-kader-algemeen">
         <h1>Laatste nieuwtjes</h1>
-        <div id="body-nieuws">
-            <div id="nieuwtjes">
-                <div class="nieuwtje">
-                    <h2>VALENTIJN MENU 2025</h2>
-                    <br>
-                    <p>Valentijn staat voor de deur. Ook dit jaar bieden we een lekker valentijnsmenu aan in onze taverne:</p>
-                    <br>
-                    <img src="{{ asset('images/valentijnmenu.jpg') }}" alt="">
-                </div>
-                <div class="nieuwtje">
-                    <h2>FEESTMENU 2024</h2>
-                    <br> 
-                    <p>Met de feestdagen zorgen wij natuurlijk ook voor een aangepast menu:</p>
-                    <br>
-                    <img src="{{ asset('images/feestmenu.jpg') }}" alt="">
-                </div>
+        <div id="nieuwtjes">
+            <div class="nieuwtje">
+                <h2>VALENTIJN MENU 2025</h2>
+                <br>
+                <p>Valentijn staat voor de deur. Ook dit jaar bieden we een lekker valentijnsmenu aan in onze taverne:</p>
+                <br>
+                <img src="{{ asset('images/valentijnmenu.jpg') }}" alt="">
+            </div>
+            <div class="nieuwtje">
+                <h2>FEESTMENU 2024</h2>
+                <br> 
+                <p>Met de feestdagen zorgen wij natuurlijk ook voor een aangepast menu:</p>
+                <br>
+                <img src="{{ asset('images/feestmenu.jpg') }}" alt="">
             </div>
         </div>
     </div>
