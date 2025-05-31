0d70cae9afd9c684368cf0d4f531d82201010f30
Jitse Lambrichts <lambrichts.j.99@gmail.com>
2025-05-31 15:58:36 +0200

    Maaltijden controller in orde

--- app/Http/Controllers/MaaltijdController.php
+++ app/Http/Controllers/MaaltijdController.php
@@ -6,6 +6,7 @@ use App\Models\Maaltijd;
 
 class MaaltijdController extends Controller
 {
+    // Alleen index om te maaltijden weer te geven
     public function index() {
         $maaltijden = Maaltijd::all();
         return view('menu', compact('maaltijden'));
