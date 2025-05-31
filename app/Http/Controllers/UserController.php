59db17c084696c9f82b928779c9cd91e5a23f538
Jitse Lambrichts <lambrichts.j.99@gmail.com>
2025-05-31 16:56:59 +0200

    UserController in orde

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,34 +6,32 @@ use App\Models\User;
 use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Session;
 use Illuminate\Validation\ValidationException;
 
 class UserController extends Controller
 {
     public function show(Request $request)
     {
-        //throws validation exception redirect back to page which requested it -> and passed errors into it
+        // Gaat controleren of de waardes voldoen aan de "voorwaardes"
         $validated = $request->validate([
             'email' => 'required|email',
             'password' => 'required|string',
         ]);
 
+        // User inloggen
         if (Auth::attempt($validated)) {
             $request->session()->regenerate();
-            Session::put([
-                'key1' => 'value1',
-                'key2' => 'value2'
-            ]);
-
+            
             return redirect()->route('mijn-account')->with('status', 'Succesvol ingelogd!');
         }
 
+        // Als er een error is, deze error weergeven
         throw ValidationException::withMessages([
             'credentials' => 'Sorry, incorrect credentials',
         ]);
     }
 
+    // Een user opslaan
     public function store(Request $request)
     {
         $validated = $request->validate([
@@ -43,22 +41,32 @@ class UserController extends Controller
         ]);
 
         try {
-            $user = User::create($validated);
+            // Gegevens mappen naar een User object
+            $user = User::create([
+                'name' => $validated['name'],
+                'email' => $validated['email'],
+                'password' => $validated['password']
+            ]);
             Auth::login($user);
-            return redirect()->route('mijn-account');
+            return redirect()->route('mijn-account')->with('status', 'Account succesvol aangemaakt!');
+
         } catch (Exception $e) {
-            return "Something went wrong:" . $e;
+            return back()->withInput()->withErrors([
+                'error' => 'Er is een fout opgetreden bij het aanmaken van je account. Probeer het opnieuw.'
+            ]);
         }
     }
     
+    // Een gebruiker uitloggen
     public function logout(Request $request) {
         Auth::logout();
-        $request->session()->invalidate();      //session invalidaten -> zeggen dat de sessie niet meer geldig is
-        $request->session()->regenerateToken();
+        $request->session()->invalidate();      // Session invalidaten -> zeggen dat de sessie niet meer geldig is
+        $request->session()->regenerateToken(); // Een nieuwe 'guest' token aanmaken 
 
         return redirect()->route('login');
     }
 
+    // Dit is nodig om de reservering op de Mijn Account pagina te kunnen weergeven
     public function account() {
         $reserveringen = Auth::user()->reserveringen;
         return view('mijn-account', compact('reserveringen'));
