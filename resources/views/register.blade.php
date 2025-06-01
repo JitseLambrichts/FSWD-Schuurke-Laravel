786e3719ca93bbf377da6241da4872e9ead4ac22
Jitse Lambrichts <lambrichts.j.99@gmail.com>
2025-06-01 11:26:07 +0200

    Register nagekeken

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -5,6 +5,7 @@
         <h1>Mijn Account</h1>
         <div id="body-register">
             <h2>Registreer:</h2>
+
             @if ($errors->any())
                 <div class="error-message">
                     <ul>
@@ -15,6 +16,7 @@
                 </div>
             @endif
             <br>
+
             <form id="register-formulier" action="{{ route('create-user') }}" method="POST">
                 @csrf
                 <div class="form-group">
@@ -23,18 +25,21 @@
                         <span class="error-message">{{ $message }}</span>
                     @enderror
                 </div>
+
                 <div class="form-group">
                     <input type="text" name="email" placeholder="Email" required>
                     @error('naam')
                         <span class="error-message">{{ $message }}</span>
                     @enderror
                 </div>
+
                 <div class="form-group">
                     <input type="password" name="password" placeholder="Wachtwoord" required>
                     @error('naam')
                         <span class="error-message">{{ $message }}</span>
                     @enderror
                 </div>  
+
                 <div class="form-group">
                     <input type="password" name="password_confirmation" placeholder="Bevestig wachtwoord" required>
                     @error('naam')
@@ -42,12 +47,14 @@
                     @enderror
                 </div>
                 <br>
-                <button type="submit"> <!-- TODO Library -->
+
+                <button type="submit"> <!-- Bronvermelding UIVerse -->
                     Registreer
                     <div class="arrow-wrapper">
                         <div class="arrow"></div>
                     </div>
-                </button> <!-- Library -->
+                </button>
+                
             </form>
         </div>
     </div>
